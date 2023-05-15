028f5f3ad71671bdb643f40dec13579bdf468e58
Jose Nuñez <jnunyez@users.noreply.github.com>
2023-05-15 18:16:44 +0100

    analyzer phase offset time error

---
+++ src/vse_sync_pp/analyzers/ppsdpll.py
@@ -0,0 +1,33 @@
+### SPDX-License-Identifier: GPL-2.0-or-later
+
+"""Analyze ppsdpll log messages"""
+
+from .analyzer import Analyzer
+
+### TODO
+#prtc_b = 40
+#prtc_a = 100
+
+class PhaseOffsetTimeErrorAnalyzer(Analyzer):
+    """Analyze DPLL phase offset time error"""
+    id_ = 'ppsdpll/phase-offset-time-error'
+    parser = 'dpll/phase-offset'
+    cols = ('phaseoffset',)
+    def prepare(self, rows):
+        return ((float(r[0]),) for r in rows)
+    def test(self, data):
+        pho_min = data.phaseoffset.min()
+        pho_max = data.phaseoffset.max()
+        return max(abs(pho_min), abs(pho_max)) < 40
+    def explain(self, data):
+        pho_min = data.phaseoffset.min()
+        pho_max = data.phaseoffset.max()
+        return {
+            'units': self._units,
+            'min': pho_min,
+            'max': pho_max,
+            'range': pho_max - pho_min,
+            'mean': data.phaseoffset.mean(),
+            'stddev': data.phaseoffset.std(),
+            'variance': data.phaseoffset.var(),
+        }
