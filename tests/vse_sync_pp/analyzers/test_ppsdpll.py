00d7db269749f3ab47805705a8e8b4e854c7abf2
Jose Nuñez <jnunyez@users.noreply.github.com>
2023-05-25 20:27:32 +0200

    unit test for phase offset constant time error

--- tests/vse_sync_pp/analyzers/test_ppsdpll.py
+++ tests/vse_sync_pp/analyzers/test_ppsdpll.py
@@ -7,7 +7,8 @@ from collections import namedtuple
 from decimal import Decimal
 
 from vse_sync_pp.analyzers.ppsdpll import (
-    PhaseOffsetTimeErrorAnalyzer
+    PhaseOffsetTimeErrorAnalyzer,
+    PhaseOffsetConstantTimeErrorAnalyzer
 )
 
 from .test_analyzer import AnalyzerTestBuilder
@@ -18,6 +19,65 @@ class TestPhaseOffsetTimeErrorAnalyzer(TestCase, metaclass=AnalyzerTestBuilder):
     """Test cases for vse_sync_pp.analyzers.ppsdpll.PhaseOffsetTimeErrorAnalyzer"""
     constructor = PhaseOffsetTimeErrorAnalyzer
     id_ = 'ppsdpll/phase-offset-time-error'
+    parser = 'dpll/phase-offset'
+    expect = (
+        (
+            (
+                DPO(Decimal(1)),
+                DPO(Decimal(1)),
+                DPO(Decimal(1)),
+            ),
+            True,
+            {
+                'units': 'ns',
+                'min': 1,
+                'max': 1,
+                'range': 0,
+                'mean': 1,
+                'stddev': 0,
+                'variance': 0,
+            },
+        ),
+        (
+            (
+                DPO(Decimal(-40)),
+                DPO(Decimal(-39)),
+                DPO(Decimal(-38)),
+            ),
+            False,
+            {
+                'units': 'ns',
+                'min': -40,
+                'max': -38,
+                'range': 2,
+                'mean': -39,
+                'stddev': 1,
+                'variance': 1,
+            },
+        ),
+        (
+            (
+                DPO(Decimal(38)),
+                DPO(Decimal(39)),
+                DPO(Decimal(40)),
+            ),
+            False,
+            {
+                'units': 'ns',
+                'min': 38,
+                'max': 40,
+                'range': 2,
+                'mean': 39,
+                'stddev': 1,
+                'variance': 1,
+            },
+        ),
+    )
+
+class TestPhaseOffsetConstantTimeErrorAnalyzer(TestCase, metaclass=AnalyzerTestBuilder):
+    """Test cases for vse_sync_pp.analyzers.ppsdpll.PhaseOffsetConstantTimeErrorAnalyzer"""
+    constructor = PhaseOffsetConstantTimeErrorAnalyzer
+    id_ = 'ppsdpll/phase-offset-constant-time-error'
     parser = 'dpll/phase-offset'
     expect = (
         (
